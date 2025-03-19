c86efd4e3053833d4e3c5eb946f2fe94f0b34fd3
doan.le <117817950+doanla-kafi@users.noreply.github.com>
2025-03-19 15:29:21 +0700

    Prepare for production - Update for production

--- docker/production/setup_static_dirs.sh
+++ docker/production/setup_static_dirs.sh
@@ -7,9 +7,12 @@ set +a
 # Create directories
 sudo mkdir -p ${PROJECT_STATIC_ROOT}
 sudo mkdir -p ${PROJECT_MEDIA_ROOT}
+sudo mkdir -p ${APP_LOGS_PATH}
 
 # Set permissions
 sudo chown -R www-data:www-data ${PROJECT_STATIC_ROOT}
 sudo chown -R www-data:www-data ${PROJECT_MEDIA_ROOT}
+sudo chown -R www-data:www-data ${APP_LOGS_PATH}
 sudo chmod -R 755 ${PROJECT_STATIC_ROOT}
-sudo chmod -R 755 ${PROJECT_MEDIA_ROOT} 
+sudo chmod -R 755 ${PROJECT_MEDIA_ROOT}
+sudo chmod -R 755 ${APP_LOGS_PATH} 
